1433f93a64cf818716cdba4935973079292ed3b7
Nafeesa Kardigudda <nafeesakardigudda786@gmail.com>
2025-05-28 18:33:59 +0530

    Frontend form add todo completed

--- todo-list-frontend/src/components/TodoForm.js
+++ todo-list-frontend/src/components/TodoForm.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React,{use, useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../features/todos/todosSlice';
 
 function TodoForm() {
+  const [title,setTitle]=useState('');
+  const [description,setDescription]=useState('');
+  const dispatch=useDispatch();
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(title.trim()){
+      dispatch(addTodo({title,description}))
+      setTitle('')
+      setDescription('')
+    }
+  }
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit} className='todo-form'>
+      <input
+      type="text"
+      placeholder='Add new todo title'
+      value={title}
+      onChange={(e)=>setTitle(e.target.value)}
+      required
+      />
+      <textarea 
+      placeholder='Description (optional)'
+      value={description}
+      onChange={(e)=>setDescription(e.target.value)}
+      ></textarea>
+      <button type='submit'>Add Todo</button>
+    </form>
   )
 }
 
--- todo-list-frontend/src/components/TodoItem.js
+++ todo-list-frontend/src/components/TodoItem.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {useDispatch} from 'react-redux';
+import {updateTodo,deleteTodo} from '../features/todos/todosSlice'
 
 function TodoItem() {
+  const [isEditing,setIsEditing]=useState(false);
+  const [editedTitle,setEditedTitle]=useState(todo.title)
   return (
     <div>
       
